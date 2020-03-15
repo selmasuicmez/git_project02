52b735899c26c14ca8d70edc718e38e049c2c85a
Selma Suicmez <selmasuicmez@gmail.com>
2020-03-15 21:41:40 +0100

    third commit

--- src/gitPackageOne/GitDay3.java
+++ src/gitPackageOne/GitDay3.java
@@ -9,6 +9,7 @@ public class GitDay3 {
 		System.out.println("my first changes on local master");
 System.out.println("this is my futre branh");
 System.out.println("My second push from local feature branch");
+System.out.println("My third push from local feature branch");
 	}
 
 }
